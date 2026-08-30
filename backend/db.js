8a12d8e980cadf07d1e7b9da95ae5fe784af5b23
choitichatterjee <choitichatterjee5@gmail.com>
2026-08-30 22:53:19 +0530

    Debug database startup

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,7 @@
+console.log("DB HOST:", process.env.DB_HOST);
+console.log("DB PORT:", process.env.DB_PORT);
+console.log("CA FILE:", path.join(__dirname, "ca.pem"));
+console.log("CA EXISTS:", fs.existsSync(path.join(__dirname, "ca.pem")));
 const mysql = require("mysql2/promise");
 const path = require("path");
 const fs = require("fs");
